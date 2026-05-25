2dc914dde8921519dfc72bfebf353b2b9dc04b62
HanlinS <106632124+gooosey@users.noreply.github.com>
2026-05-26 00:24:10 +1200

    Add files via upload

--- Project5/data_sources.R
+++ Project5/data_sources.R
@@ -1,24 +1,27 @@
-#library(tidyverse)
-#library(rvest)
-#library(jsonlite)
+library(tidyverse)
+library(rvest)
+library(jsonlite)
 
-# Find me list of files
+# ---- Load HTML files ----
 listfiles <- list.files("html", full.names = TRUE)
 
+# ---- Load scraping function ----
 source("scrape_html.R")
 
-# bee dataset
+# ---- Create Beehive dataset ----
 beehive <- map_df(listfiles, scrape_search_results) %>% distinct()
 saveRDS(beehive, "beehive.rds")
 
+# ---- Load Wikipedia infobox function ----
 source("get_wikipedia_infobox.R")
 
+# ---- Create minister vector ----
 minister_names <- beehive %>%
   separate_rows(ministers, sep = ";") %>%
-  pull(ministers) %>% 
+  pull(ministers) %>%
   unique()
 
-
+# ---- Create ministers dataset from Wikipedia ----
 ministers <- map_df(minister_names, get_wikipedia_infobox)
 
 saveRDS(ministers, "ministers.rds")
